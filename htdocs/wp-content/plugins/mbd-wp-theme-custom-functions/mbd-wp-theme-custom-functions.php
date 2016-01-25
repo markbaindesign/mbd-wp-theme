67b3297ea6e51115a4ed6eb0d3a9fab81cf52718
markbain <markbaindesign@gmail.com>
2016-01-25 16:33:36 +0100

    add customs functions plugin code

--- htdocs/wp-content/plugins/mbd-wp-theme-custom-functions/mbd-wp-theme-custom-functions.php
+++ htdocs/wp-content/plugins/mbd-wp-theme-custom-functions/mbd-wp-theme-custom-functions.php
@@ -6,3 +6,112 @@
  Version: 0.0.0
  Author URI: http://markbaindesign.com
  */
+
+ include_once('inc/CPT.php');
+ include_once("inc/Tax-meta-class/Tax-meta-class.php");
+
+ // Shortcodes
+ include_once('inc/shortcodes/twitter-feed.php');
+ include_once('inc/shortcodes/buttons.php');
+
+ // Clinics CPT
+
+ // create clinics custom post type
+ $work = new CPT(array(
+     'post_type_name' => 'work',
+     'singular' => 'Work',
+     'plural' => 'Work',
+     'slug' => 'work'
+ ), array(
+     'supports' => array( 'author', 'title', 'editor', 'thumbnail', 'excerpt', 'comments'),
+     'has_archive'        => true,
+     'capability_type'    => 'post',
+     'show_in_menu'       => true,
+ ));
+
+ // use  icon for post type
+ $work->menu_icon("dashicons-store");
+
+ $work->register_taxonomy( 'type' );
+ $work->register_taxonomy( 'tag' );
+
+
+ /* MBD Custom Latest Posts Shortcode */
+
+ add_filter('widget_text', 'do_shortcode'); // Enable shortcodes in text widgets
+
+ add_shortcode('mbd-latest-posts', 'mbdmaster324_shortcode_latestposts');
+
+ function mbdmaster324_shortcode_latestposts( $atts ) {
+ ob_start();
+     $query = new WP_Query( array(
+         'posts_per_page' => 3,
+     ) );  
+     if ( $query->have_posts() ) { ?>
+             <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+             	<div class="home-latest-posts">
+ 					<div id="home-latest-posts-thumb">
+ 						<?php if ( has_post_thumbnail() ) {
+ 							the_post_thumbnail('markbaindesign-sq-50');
+ 						} else { ?>
+ 							<img src="<?php bloginfo('stylesheet_directory'); ?>/img/default-thumbnail.png" alt="<?php the_title(); ?>" />
+ 						<?php } ?>
+ 					</div>
+ 					<div class="home-latest-posts-title">
+ 						<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+ 					</div>
+ 					<span id="home-latest-posts-author" class="home-meta">By <?php the_author_posts_link(); ?></span>
+ 					<span id="home-latest-posts-date" class='home-meta'><?php the_time('j F, Y'); ?></span>
+ 					<div id="home-latest-posts-comments" class="home-meta"><?php comments_number( 'no responses', 'one response', '% responses' ); ?></div>
+ 				</div>
+ 			<?php endwhile; ?>
+ 			<span class="read-more">
+ 				<a class="button" href="<?php bloginfo( 'url' ) ?>/blog/">See more blog posts</a>
+ 			</span>
+             <?php wp_reset_postdata(); ?>
+         </div>
+     <?php $myvariable = ob_get_clean();
+     return $myvariable;
+     }
+ }
+
+ // [mbd-latest-posts]
+
+ if (is_admin()){
+   /* 
+    * prefix of meta keys, optional
+    */
+   $prefix = 'mbd_';
+   /* 
+    * configure your meta box
+    */
+   $config = array(
+     'id' => 'images_meta_box',          // meta box id, unique per meta box
+     'title' => 'Images',          // meta box title
+     'pages' => array('type'),        // taxonomy name, accept categories, post_tag and custom taxonomies
+     'context' => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+     'fields' => array(),            // list of meta fields (can be added by field arrays)
+     'local_images' => true,          // Use local or hosted images (meta box images for add/remove)
+     'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+   );
+   
+   
+   /*
+    * Initiate your meta box
+    */
+   $my_meta =  new Tax_Meta_Class($config);
+   
+   /*
+    * Add fields to your meta box
+    */
+
+   
+   //Image fields
+   $my_meta->addImage($prefix.'thumb_image_field_id',array('name'=> __('Grid Thumbnail Image ','tax-meta')));
+   
+   /*
+    * Don't Forget to Close up the meta box decleration
+    */
+   //Finish Meta Box Decleration
+   $my_meta->Finish();
+ }
