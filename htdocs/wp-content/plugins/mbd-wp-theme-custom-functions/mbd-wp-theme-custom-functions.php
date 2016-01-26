a5f56cecdc70a79bf084ce2bcdd7733ee8168cda
markbain <markbaindesign@gmail.com>
2016-01-26 12:07:08 +0100

    add types and tags to work cpt

--- htdocs/wp-content/plugins/mbd-wp-theme-custom-functions/mbd-wp-theme-custom-functions.php
+++ htdocs/wp-content/plugins/mbd-wp-theme-custom-functions/mbd-wp-theme-custom-functions.php
@@ -29,8 +29,10 @@
      'show_in_menu'       => true,
  ));
 
- // use  icon for post type
- $work->menu_icon("dashicons-portfolio");
+    // use  icon for post type
+    $work->menu_icon("dashicons-portfolio");
+    $work->register_taxonomy( 'type' );
+    $work->register_taxonomy( 'tag' );
 
  /*
   * Testimonial CPT
@@ -93,3 +95,42 @@
  }
 
  // [mbd-latest-posts]
+
+ if (is_admin()){
+   /* 
+    * prefix of meta keys, optional
+    */
+   $prefix = 'mbd_';
+   /* 
+    * configure your meta box
+    */
+   $config = array(
+     'id' => 'images_meta_box',          // meta box id, unique per meta box
+     'title' => 'Images',          // meta box title
+     'pages' => array('type'),        // taxonomy name, accept categories, post_tag and custom taxonomies
+     'context' => 'normal',            // where the meta box appear: normal (default), advanced, side; optional
+     'fields' => array(),            // list of meta fields (can be added by field arrays)
+     'local_images' => true,          // Use local or hosted images (meta box images for add/remove)
+     'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
+   );
+   
+   
+   /*
+    * Initiate your meta box
+    */
+   $my_meta =  new Tax_Meta_Class($config);
+   
+   /*
+    * Add fields to your meta box
+    */
+
+   
+   //Image fields
+   $my_meta->addImage($prefix.'thumb_image_field_id',array('name'=> __('Grid Thumbnail Image ','tax-meta')));
+   
+   /*
+    * Don't Forget to Close up the meta box decleration
+    */
+   //Finish Meta Box Decleration
+   $my_meta->Finish();
+ }
