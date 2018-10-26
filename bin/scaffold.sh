f2aa96b46da6937d8e75068f6c46318257811490
markbain <markbaindesign@gmail.com>
2018-10-26 17:43:34 +0200

    delete bin - scripts are covered in new provision

--- bin/scaffold.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set up standard web project scaffolding
-
-mkdir -m 755 ../httpdocs ../import ../export ../sass ../release ../src
-
-chown mark:mark ../httpdocs ../import ../export ../sass ../release ../src
