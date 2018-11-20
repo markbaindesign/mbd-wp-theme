c7dd9b40a2daf2567c2dee103482ef1abf0308f7
markbain <markbaindesign@gmail.com>
2018-11-20 17:50:42 +0100

    add theme support for title tag

--- htdocs/wp-content/themes/mbd-wp-theme/framework/inc/init/add_theme_support.php
+++ htdocs/wp-content/themes/mbd-wp-theme/framework/inc/init/add_theme_support.php
@@ -1,5 +1,5 @@
 <?php
-
-add_theme_support( 'post-thumbnails' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'html5', array('search-form','comment-form','gallery','caption','comment-list') );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'title-tag' );
