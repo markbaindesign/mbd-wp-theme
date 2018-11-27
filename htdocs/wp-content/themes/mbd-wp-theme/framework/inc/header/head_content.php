ceb1e8bff1de8278a522bd864a6baab40f38a3c5
markbain <markbaindesign@gmail.com>
2018-11-27 17:57:25 +0100

    remove fa from heaf

--- htdocs/wp-content/themes/mbd-wp-theme/framework/inc/header/head_content.php
+++ htdocs/wp-content/themes/mbd-wp-theme/framework/inc/header/head_content.php
@@ -5,7 +5,5 @@ if ( ! function_exists( 'baindesign324_head_content' ) ) :
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<style type="text/css">.js #flash {display: none;}</style>
 		<script type="text/javascript">document.documentElement.className = 'js';</script>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css">
-  		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/v4-shims.css">
 	<?php }
 endif;
