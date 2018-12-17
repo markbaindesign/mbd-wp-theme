3d7e3e19031679641a5b54036cde468f8043b790
markbain <markbaindesign@gmail.com>
2018-12-17 18:14:46 +0100

    restore theme

--- htdocs/wp-content/themes/mbd-wp-theme/functions.php
+++ htdocs/wp-content/themes/mbd-wp-theme/functions.php
@@ -8,4 +8,4 @@ require get_template_directory() . '/framework/framework.php';
 /**
  * Load the custom theme if it exists
  */
-// include get_template_directory() . '/custom/custom.php';
+include get_template_directory() . '/custom/custom.php';
