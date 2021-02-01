f13029caf7657aeed30fcaba6a6fb739a49153bf
markbain <markbaindesign@gmail.com>
2021-02-01 17:17:51 +0100

    changes to framework from TSFT updates

--- public_html/wp-content/themes/mbd-wp-theme/framework/inc/misc/misc.php
+++ public_html/wp-content/themes/mbd-wp-theme/framework/inc/misc/misc.php
@@ -6,4 +6,5 @@ require get_template_directory() . '/framework/inc/misc/custom-meta.php';
 require get_template_directory() . '/framework/inc/misc/social-links.php';
 require get_template_directory() . '/framework/inc/misc/offcanvas-nav.php';
 require get_template_directory() . '/framework/inc/misc/template-tags.php';
+require get_template_directory() . '/framework/inc/misc/theme-version.php';
 require get_template_directory() . '/framework/inc/misc/searchform-fullscreen.php';
---
+++ public_html/wp-content/themes/mbd-wp-theme/framework/inc/misc/theme-version.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Theme version
+ */
+
+if (!function_exists('baindesign324_theme_version')) :
+   function baindesign324_theme_version()
+   { 
+      // Value updated via Grunt task when 
+      // theme version changes. 
+      // Used to get version in stylesheet enqueue function
+      // for cache-busting.
+      $version="1.8.0";
+      return $version;
+    }
+endif;
