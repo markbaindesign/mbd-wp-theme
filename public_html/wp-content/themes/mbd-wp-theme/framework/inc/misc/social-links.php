048bb30ac043e70a603904f3ece7bc09bdf3bf6f
markbain <markbaindesign@gmail.com>
2021-07-07 17:28:46 +0200

    Refactor

--- public_html/wp-content/themes/mbd-wp-theme/framework/inc/misc/social-links.php
+++ public_html/wp-content/themes/mbd-wp-theme/framework/inc/misc/social-links.php
@@ -2,19 +2,24 @@
 
 if ( ! function_exists( 'baindesign324_social_links' ) ) :
 	function baindesign324_social_links() {
+		if( bd324_get_social_links() ) {
+			print bd324_get_social_links();
+		}
+	}
+endif;
 
-	    if( have_rows('social_media_links', 'option') ):
-	        echo '<ul class="social-media-links">';
+if ( ! function_exists( 'bd324_get_social_links' ) ) :
+	function bd324_get_social_links() {		
+		$content = '';
+		 if( have_rows('social_media_links', 'option') ):
+			$content.= '<ul class="icon__ul icon__ul--social social-media-links">';
 	        while ( have_rows('social_media_links', 'option') ) : the_row();
-	            $content='<li><a href="' . get_sub_field( 'account_url','option' ) . '" ';
+	            $content.='<li class="icon__li icon__li--social" ><a class="icon__link icon__link--social" href="' . get_sub_field( 'account_url','option' ) . '" ';
 	            $content.='"title="'.get_sub_field( 'service_name','option' ).'" ';
-	            $content.='"><i class="social-media-links__icon fab '.get_sub_field( 'icon_class','option' ).'"></i><span class="social-media-links__label">'.get_sub_field( 'service_name','option' ).'</span></a></li>';
-	            echo $content;
+	            $content.='"><i class="icon__i icon__i--social social-media-links__icon fab '.get_sub_field( 'icon_class','option' ).'"></i><span class="social-media-links__label">'.get_sub_field( 'service_name','option' ).'</span></a></li>';
 	        endwhile;
-	        echo '</ul>';
-	    else :
-	        echo '<!-- No social media accounts found! -->';
-	        echo '<!-- Add an account via Theme Settings -->';
-	    endif;
+	        $content.= '</ul>';
+		 endif;
+		 return $content;
 	}
 endif;
