fa2bd94fa8cbb3dcac91ee7855c14911acc3de0a
Mark Bain <mark@bain.design>
2024-01-26 11:06:14 +0100

    remove obsolete scripts

--- provision/scripts/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Nginx
-cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-sed -i "s#{{WEBROOT_HERE}}#${WEBROOT}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
